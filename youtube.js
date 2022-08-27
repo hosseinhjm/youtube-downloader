ca77168ac75d188b6cd32941b47a66875f7863ce
hosseinhjm <67948521+hosseinhjm@users.noreply.github.com>
2022-08-27 20:40:49 +0430

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,15 @@
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) {
+    fetch(`http://localhost:4000/download?URL=${URL}`, {
+        method:'GET'
+    }).then(res => res.json())
+    .then(json => console.log(json));
+}
+function sendURL(URL) {
+    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+}
